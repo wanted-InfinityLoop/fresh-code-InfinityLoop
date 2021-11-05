cfe63f04f2a928e0e8cf7a4972bf3588abc3792a
YeJun <hayejun1013@naver.com>
2021-11-05 19:23:55 +0900

    feat: add Product create method
    - 상품 등록 기능 구현
    - 추후 유닛테스트, 어드민 계정만 CUD 할 수 있도록 구현 예정
    
    refactor: refactor unused code

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .           import views
 
 urlpatterns = [
-    path("/<int:menu_id>", views.MenuDetailView.as_view())
+    path("", views.MenuDetailView.as_view()),
+    path("/<int:menu_id>", views.MenuDetailView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -3,10 +3,31 @@ import json
 from rest_framework.views import APIView
 from django.http import JsonResponse
 
-from .models import Menu
+from .models import Menu, Category, Badge, Tag
 
 
 class MenuDetailView(APIView):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            category = Category.objects.get(name=data["category"])
+            badge = Badge.objects.get(name=data["badge"])
+            tag = Tag.objects.get(name=data["tag"])
+
+            menu = Menu.objects.create(
+                name        = data["name"],
+                category    = category,
+                description = data['description'],
+                badge       = badge,
+                tag         = tag
+            )
+            
+            return JsonResponse({"message": f"{menu.name} has successfully posted"}, status=201)
+            
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
     def delete(self, request, menu_id):
         try:
             menu = Menu.objects.get(id=menu_id)
