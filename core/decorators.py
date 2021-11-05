f1372154a10d897ee6436fec349fdc7948420efb
Chi heon Song <hrpp1353@gmail.com>
2021-11-05 20:47:34 +0900

    chore: modify convention
    - 컨벤션 안맞는 것들 수정
    - 안쓰는 모듈 삭제제

--- core/decorators.py
+++ core/decorators.py
@@ -2,8 +2,8 @@ from jwt import exceptions, decode
 
 from django.http import JsonResponse
 
-from my_settings import MY_SECRET_KEY
-from users.models import User, Role
+from my_settings  import MY_SECRET_KEY
+from users.models import User
 
 
 def authorizer(func):
@@ -12,7 +12,7 @@ def authorizer(func):
             access_token = request.headers.get("Authorization")
             
             if not access_token:
-                return JsonResponse({"message": "Not Found Access_token"}, status = 403)
+                return JsonResponse({"message": "Not Found Access_token"}, status=403)
             
             payload = decode(access_token, MY_SECRET_KEY, algorithm="HS256")
             user_id = payload["id"]
@@ -21,19 +21,19 @@ def authorizer(func):
             user = User.objects.get_or_none(id=user_id)
 
             if not user:
-                return JsonResponse({"message": "User Not Found"}, status = 404)
+                return JsonResponse({"message": "User Not Found"}, status=404)
 
             if user.role_id != role_id:
-                return JsonResponse({"message": "Invalid User"}, status = 401)
+                return JsonResponse({"message": "Invalid User"}, status=401)
 
             request.user = user
             request.role = role_id
 
         except exceptions.DecodeError:
-            return JsonResponse({"message": "Invalid Token"}, status = 403)
+            return JsonResponse({"message": "Invalid Token"}, status=403)
 
         except exceptions.ExpiredSignatureError:
-            return JsonResponse({"message": "Token Expired"}, status = 403)
+            return JsonResponse({"message": "Token Expired"}, status=403)
 
         return func(self, request, *args, **kwagrs)
 
