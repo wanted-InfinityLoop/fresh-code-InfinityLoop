ce8af4d4a9ea991ab7fa94d626f21d7bab731389
YeJun <hayejun1013@naver.com>
2021-11-05 13:50:18 +0900

    feat: add users and products model
    - 유저 모델 작성
    - 상품 모델 작성

--- products/models.py
+++ products/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = "categories"
+
+
+class Badge(models.Model):
+    name = models.CharField(max_length=16)
+
+    class Meta:
+        db_table = "badges"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+    type = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = "tags"
+
+
+class Menu(models.Model):
+    name        = models.CharField(max_length=32)
+    category    = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=128)
+    badge       = models.ForeignKey(Badge, on_delete=models.PROTECT, null=True)
+    is_sold     = models.BooleanField(default=False)
+    tag         = models.ForeignKey(Tag, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "menus"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=16)
+    size = models.CharField(max_length=16)
+
+    class Meta:
+        db_table = "sizes"
+
+
+class Item(models.Model):
+    menu    = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    size    = models.ForeignKey(Size, on_delete=models.PROTECT)
+    price   = models.PositiveIntegerField()
+    is_sold = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "items"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    class Type(models.IntegerChoices):
+        ADMIN    = 1
+        CUSTOMER = 2
+
+    name = models.CharField(max_length=16)
+
+    class Meta:
+        db_table = "roles"
+
+
+class User(models.Model):
+    name         = models.CharField(max_length=32, null=True)
+    email        = models.CharField(max_length=64, unique=True)
+    password     = models.CharField(max_length=128, null=True)
+    role         = models.ForeignKey(Role, on_delete=models.PROTECT)
+
+    class Meta:
+        db_table = "users"
